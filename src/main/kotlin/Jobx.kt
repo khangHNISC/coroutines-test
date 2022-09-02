a4ffa6123a8ed850e8d75de566c74bdf6ddf96ab
Khang Le <KhangLD5@fpt.com.vn>
2022-09-02 19:17:57 +0700

    job

---
+++ src/main/kotlin/Jobx.kt
@@ -0,0 +1,15 @@
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    val job = launch { // launch a new coroutine and keep a reference to its Job
+        delay(1000L)
+        println("World!")
+    }
+    println("Hello")
+    job.join() // wait until child coroutine completes
+    println("Done")
+}
+
+//launch is a coroutine builder that execute concurrently and return a job
