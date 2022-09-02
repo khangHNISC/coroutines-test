a25f42380607e7cf1a15b526a3aa9fefdb5b008e
Khang Le <KhangLD5@fpt.com.vn>
2022-09-02 14:48:58 +0700

    suspend coroutine scope

--- src/main/kotlin/Basic.kt
+++ src/main/kotlin/Basic.kt
@@ -1,13 +1,20 @@
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking { // this: CoroutineScope
-    launch { doWorld() }
+    doWorld()
+}
+
+private suspend fun doWorld() = coroutineScope {
+    launch {
+        delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
+        println("World!") // print after delay
+    }
     println("Hello") // main coroutine continues while a previous one is delayed
 }
 
-private suspend fun doWorld() {
-    delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
-    println("World!") // print after delay
-}
+//launch is a coroutine builder. It launches a new coroutine concurrently, work independently
+//delay is suspending function does not block the underlying thread but allows other coroutines to run and use the underlying thread
+//runblocking is also a coroutine builder that bridges the non-coroutine to coroutine.
